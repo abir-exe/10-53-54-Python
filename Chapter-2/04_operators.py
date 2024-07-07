61ba4c24212761bafd6088f0afb4181384756d84
abir-exe <abirmahmud221@gmail.com>
2024-07-07 13:53:20 +0600

    chapter 2 finished

---
+++ Chapter-2/04_operators.py
@@ -0,0 +1,23 @@
+# Arithmatic Operators
+
+a =7
+b=4
+c=a+b
+print(c)
+
+# Assignment Operators
+a = 4-2
+print(a)
+b= 6
+b+=3
+print(b)
+
+# Comparison Operators
+
+d = 5!=7
+print(d)
+
+# Logical Operator
+
+e = True or False
+print(e)
---
+++ Chapter-2/Chapter-2-PS/02_problem2.py
@@ -0,0 +1,3 @@
+a = 34
+b =5
+print("The remainder when a is divided by b is",a%b)
