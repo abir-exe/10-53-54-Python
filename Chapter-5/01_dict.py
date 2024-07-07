533ac061ce158d4eb0b0c3e20b55b0af0e7b74eb
abir-exe <abirmahmud221@gmail.com>
2024-07-07 22:17:42 +0600

    chapter 5 finished except the PS

---
+++ Chapter-5/01_dict.py
@@ -0,0 +1,10 @@
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23
+}
+
+marks = [["Harry", 100], "Abir"]
+
+# print(marks, type(marks))
+print(marks["Harry"])
---
+++ Chapter-5/03_sets.py
@@ -0,0 +1,5 @@
+e = set() # Do not use s = {} as it will create an empty dictionary
+
+s = {1, 5, 32, 5, 3, 5}
+
+print(s)
